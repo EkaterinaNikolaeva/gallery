bd99a20f879ea55beaed36405a3bf1febafb273a
EkaterinaNikolaeva <57720667+EkaterinaNikolaeva@users.noreply.github.com>
2020-04-30 00:36:52 +0300

    Create l.py

---
+++ data/l.py
@@ -0,0 +1,16 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+
+from .db_session import SqlAlchemyBase
+
+
+class Likes(SqlAlchemyBase):
+    __tablename__ = 'likes'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, 
+                           primary_key=True, autoincrement=True)
+    picture = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, 
+                                sqlalchemy.ForeignKey("users.id"))
+    user = orm.relation('User')
